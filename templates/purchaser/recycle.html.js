0b5a102def0b04f8a4cbf4a41ba8c18165182b9c
Asia <15901212106@163.com>
2017-06-15 17:19:51 +0800

    个人回收页面

---
+++ templates/purchaser/recycle.html.js
@@ -0,0 +1,51 @@
+/**
+ * Created by ASia on 2017-06-15.
+ */
+$('.btn-group>.btn').click(function() {
+    var parentEle = $(this).parent();
+    if (parentEle.hasClass('single-check')) {
+        parentEle.find('.btn').each(function() {
+            $(this).removeClass('active');
+            $(this).removeClass('btn-success');
+            $(this).addClass('btn-default');
+        });
+        $(this).addClass('active');
+        $(this).removeClass('btn-default');
+        $(this).addClass('btn-success');
+    } else {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).addClass('active');
+        }
+        if ($(this).hasClass('btn-success')) {
+            $(this).removeClass('btn-success');
+        } else {
+            $(this).addClass('btn-success');
+        }
+    }
+});
+$("#menu .btn").click(function () {
+    if($("#fun_1").hasClass('active')){
+        $(".fun_1").show(500);
+        $("#next").show(500);
+        $(".panel-body").hide(500);
+        $(".fun_3").hide(500)
+    }
+    if($("#fun_2").hasClass('active')){
+        $(".panel-body").show(500);
+        $(".username").css('display','none');
+        $(".anonymous").css('display','block');
+        $(".fun_1").hide(500);
+        $(".fun_3").hide(500)
+    }
+
+
+    if($("#fun_3").hasClass('active')){
+        $(".panel-body").hide(400);
+        $(".fun_1").hide(500);
+        $(".fun_3").show(500)
+    }
+});
+
+
