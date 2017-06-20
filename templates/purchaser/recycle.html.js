21d950e38d3b895b0c5445ca5b5f09a19a00bfa7
Asia <15901212106@163.com>
2017-06-20 17:46:41 +0800

    个人回收页面

--- templates/purchaser/recycle.html.js
+++ templates/purchaser/recycle.html.js
@@ -28,24 +28,24 @@ $('.btn-group>.btn').click(function() {
 $("#menu .btn").click(function () {
     if($("#fun_1").hasClass('active')){
         var a = $("#fun_1").val();
-        $(".fun_1").show(500);
-        $("#next").show(500);
-        $(".panel-body").hide(500);
-        $(".fun_3").hide(500)
+        $(".fun_1").show();
+        $("#next").show();
+        $(".userinfo").hide();
+        $(".fun_3").hide()
     }
     if($("#fun_2").hasClass('active')){
         var a = $("#fun_2").val();
-        $(".panel-body").show(500);
+        $(".userinfo").show();
         $(".username").css('display','none');
         $(".anonymous").css('display','block');
-        $(".fun_1").hide(500);
-        $(".fun_3").hide(500)
+        $(".fun_1").hide();
+        $(".fun_3").hide()
     }
     if($("#fun_3").hasClass('active')){
         var a = $("#fun_3").val();
-        $(".panel-body").hide(400);
-        $(".fun_1").hide(500);
-        $(".fun_3").show(500)
+        $(".userinfo").hide();
+        $(".fun_1").hide();
+        $(".fun_3").show()
     }
 });
 
